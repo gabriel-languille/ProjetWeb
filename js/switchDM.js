ef0109d706dcd04e827f7b3a48e7630d891193d4
LrnaKiut <antoine.welter@etu.univ-amu.fr>
2021-11-20 20:32:07 +0100

    footer + style dp

---
+++ js/switchDM.js
@@ -0,0 +1,12 @@
+function swapStyleSheet(sheet) {
+    document.getElementById("pagestyle").setAttribute("href", sheet);  
+}
+
+function initate() {
+    var style1 = document.getElementById("stylesheet1");
+    var style2 = document.getElementById("stylesheet2");
+
+    style1.onclick = function () { swapStyleSheet("CSS/css1DM.css") };
+    style2.onclick = function () { swapStyleSheet("CSS/css2DM.css") }; 
+
+}
